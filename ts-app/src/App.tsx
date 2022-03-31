57fe9dfb871b67224b3fc69760c21ae2f746644b
violetwhenisawu <anashape710@gmail.com>
2022-03-31 22:48:41 +0900

    Typescript State, Event 실습

--- ts-app/src/App.tsx
+++ ts-app/src/App.tsx
@@ -1,10 +1,26 @@
-import Circle from "./Circle";
+// import Circle from "./Circle";
+
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const handleChange = (e:React.FormEvent<HTMLInputElement>) => {
+    const {currentTarget : {value}, } = e;
+    setValue(value);
+  };
+  
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(`hello ${value}`);
+  };
   return (
     <div>
-      <Circle borderColor="yellow" bgColor="teal"/>
-      <Circle bgColor="tomato"/>
+      {/* <Circle borderColor="yellow" bgColor="teal"/>
+      <Circle bgColor="tomato"/> */}
+      <form onSubmit={handleSubmit}>
+        <input value={value} onChange={handleChange} type="text" placeholder="username" />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
