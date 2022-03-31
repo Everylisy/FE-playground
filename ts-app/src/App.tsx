7d5b4abc053be5fb3857cc1032cd52eaae8d8347
violetwhenisawu <anashape710@gmail.com>
2022-03-31 23:34:16 +0900

    theme 다크모드 실습

--- ts-app/src/App.tsx
+++ ts-app/src/App.tsx
@@ -1,26 +1,40 @@
 // import Circle from "./Circle";
 
 import React, { useState } from "react";
+import styled from "styled-components";
 
 function App() {
-  const [value, setValue] = useState("");
-  const handleChange = (e:React.FormEvent<HTMLInputElement>) => {
-    const {currentTarget : {value}, } = e;
-    setValue(value);
-  };
+
+  const Container = styled.div`
+    background-color: ${props => props.theme.bgColor}
+
+  `;
+
+  const H1 = styled.h1`
+    color: ${props => props.theme.textColor}
+  `;
+
+  // const [value, setValue] = useState("");
+  // const handleChange = (e:React.FormEvent<HTMLInputElement>) => {
+  //   const {currentTarget : {value}, } = e;
+  //   setValue(value);
+  // };
   
-  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(`hello ${value}`);
-  };
+  // const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   console.log(`hello ${value}`);
+  // };
   return (
     <div>
       {/* <Circle borderColor="yellow" bgColor="teal"/>
-      <Circle bgColor="tomato"/> */}
+      <Circle bgColor="tomato"/>
       <form onSubmit={handleSubmit}>
         <input value={value} onChange={handleChange} type="text" placeholder="username" />
         <button>Log in</button>
-      </form>
+      </form> */}
+      <Container>
+      <H1>protected!</H1>
+      </Container>
     </div>
   );
 }
