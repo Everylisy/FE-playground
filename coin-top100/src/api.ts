4667e17d21bc1ba5b58ea4d552cdb244abfbd393
violetwhenisawu <anashape710@gmail.com>
2022-10-14 02:30:20 +0900

    [Fix]: 코인 과거 데이터 API 요청 주소 변경

--- coin-top100/src/api.ts
+++ coin-top100/src/api.ts
@@ -1,4 +1,5 @@
 const BASE_URL = `https://api.coinpaprika.com/v1`;
+const NOMAD_URL = `https://ohlcv-api.nomadcoders.workers.dev`;
 
 export function fetchCoins() {
   return fetch(`${BASE_URL}/coins`).then((res) => res.json());
@@ -13,9 +14,7 @@ export function fetchCoinTickers(coinId: string | undefined) {
 }
 
 export function fetchCoinHistory(coinId: string | undefined) {
-  const endDate = Math.floor(Date.now() / 1000);
-  const startDate = endDate - 60 * 60 * 24 * 7 * 2;
-  return fetch(
-    `${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
-  ).then((res) => res.json());
+  // const endDate = Math.floor(Date.now() / 1000);
+  // const startDate = endDate - 60 * 60 * 24 * 7 * 2;
+  return fetch(`${NOMAD_URL}?coinId=${coinId}`).then((res) => res.json());
 }
