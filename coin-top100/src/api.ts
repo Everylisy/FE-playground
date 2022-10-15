598284537dae3711fff89f392c8b6118bf869111
violetwhenisawu <anashape710@gmail.com>
2022-10-16 02:01:00 +0900

    [Add]: 모든 코인 시세 요청 API 추가

--- coin-top100/src/api.ts
+++ coin-top100/src/api.ts
@@ -9,12 +9,16 @@ export function fetchCoinInfo(coinId: string | undefined) {
   return fetch(`${BASE_URL}/coins/${coinId}`).then((res) => res.json());
 }
 
+export function fetchAllCoinTickers() {
+  return fetch(`https://api.coinpaprika.com/v1/tickers`).then((res) =>
+    res.json()
+  );
+}
+
 export function fetchCoinTickers(coinId: string | undefined) {
   return fetch(`${BASE_URL}/tickers/${coinId}`).then((res) => res.json());
 }
 
 export function fetchCoinHistory(coinId: string | undefined) {
-  // const endDate = Math.floor(Date.now() / 1000);
-  // const startDate = endDate - 60 * 60 * 24 * 7 * 2;
   return fetch(`${NOMAD_URL}?coinId=${coinId}`).then((res) => res.json());
 }
