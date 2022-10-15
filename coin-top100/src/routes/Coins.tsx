c52e868bae6cad144b23b64a187023947edfd380
violetwhenisawu <anashape710@gmail.com>
2022-10-16 03:39:24 +0900

    [Modify]: 메인페이지 API 함수 변경, UI 수정

--- coin-top100/src/routes/Coins.tsx
+++ coin-top100/src/routes/Coins.tsx
@@ -3,8 +3,9 @@ import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
-import { fetchCoins } from "../api";
+import { fetchAllCoinTickers } from "../api";
 import { isDarkAtom } from "../atoms";
+import { IPriceData } from "./Coin";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -22,16 +23,14 @@ const Header = styled.header`
 const CoinsList = styled.ul``;
 
 const Coin = styled.li`
-  background-color: ${(props) => props.theme.cardBgColor};
   color: ${(props) => props.theme.textColor};
-  border-radius: 15px;
   margin-bottom: 10px;
-  border: 1px solid white;
   a {
     display: flex;
     align-items: center;
-    transition: color 0.2s ease-in;
-    padding: 20px;
+    font-weight: 500;
+    transition: color 0.2s ease-in-out;
+    padding: 10px 10px;
   }
   &:hover {
     a {
@@ -51,48 +50,54 @@ const Loader = styled.span`
   font-size: 20px;
 `;
 
+const Rank = styled.span`
+  margin-right: 20px;
+  font-size: 14px;
+  color: ${(props) => props.theme.greyTextColor};
+`;
+
 const Img = styled.img`
   width: 35px;
   height: 35px;
-  margin-right: 10px;
+  margin-right: 20px;
 `;
 
+const CoinInfo = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+`;
+
+const CoinName = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const CoinPrice = styled(CoinName)`
+  text-align: right;
+`;
+
+const Symbol = styled.span`
+  font-size: 12px;
+  font-weight: 400;
+  color: ${(props) => props.theme.greyTextColor};
+`;
+
+const Volume = styled(Symbol)``;
+
 const DarkModeBtn = styled.button`
+  display: inline-block;
   width: 130px;
   height: 40px;
-  padding: 10px 25px;
-  border: 0.5px solid ${(props) => props.theme.accentColor};
-  font-family: "Montserrat", sans-serif;
-  background: transparent;
-  cursor: pointer;
-  transition: all 0.3s ease;
-  position: relative;
-  display: inline-block;
   margin-left: auto;
-  background: ${(props) => props.theme.accentColor};
+  padding: 10px 25px;
+  font-family: "Montserrat", sans-serif;
   color: white;
-  z-index: 1;
-  &:after {
-    position: absolute;
-    content: "";
-    width: 0;
-    height: 100%;
-    top: 0;
-    right: 0;
-    z-index: -1;
-    background: whitesmoke;
-    transition: all 0.3s ease;
-  }
-  &:hover {
-    color: #000;
-  }
-  &:hover:after {
-    left: 0;
-    width: 100%;
-  }
-  &:active {
-    top: 2px;
-  }
+  background-color: ${(props) => props.theme.accentColor};
+  border: 0.5px solid ${(props) => props.theme.accentColor};
+  cursor: pointer;
 `;
 
 interface Icoin {
@@ -108,7 +113,10 @@ interface Icoin {
 function Coins() {
   const setDarkAtom = useSetRecoilState(isDarkAtom);
   const toggleDarkAtom = () => setDarkAtom((prev) => !prev);
-  const { isLoading, data } = useQuery<Icoin[]>(["allCoins"], fetchCoins);
+  const { isLoading, data: tickersData } = useQuery<IPriceData[]>(
+    ["allTickers"],
+    fetchAllCoinTickers
+  );
 
   return (
     <Container>
@@ -123,13 +131,23 @@ function Coins() {
         <Loader>Loading...</Loader>
       ) : (
         <CoinsList>
-          {data?.slice(0, 100).map((coin) => (
+          {tickersData?.slice(0, 100).map((coin) => (
             <Coin key={coin.id}>
               <Link to={`/${coin.id}`} state={coin.name}>
+                <Rank>{coin.rank}</Rank>
                 <Img
-                  src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                  src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
                 />
-                {coin.name} &rarr;
+                <CoinInfo>
+                  <CoinName>
+                    {coin.name}
+                    <Symbol>{coin.symbol}</Symbol>
+                  </CoinName>
+                  <CoinPrice>
+                    {coin.quotes.USD.price.toFixed(2)}
+                    <Volume>{coin.quotes.USD.percent_change_24h + "%"}</Volume>
+                  </CoinPrice>
+                </CoinInfo>
               </Link>
             </Coin>
           ))}
